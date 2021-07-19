e42171e86831774def6975d4bd176b2efecf34a7
Julien B <julien@bosse.pro>
2021-07-19 15:08:42 +0200

    treminé

--- cours_python/matrices.py
+++ cours_python/matrices.py
@@ -58,9 +58,60 @@ def soustraction(a,b):
 
     return c
 
+def multiplication(a,b):
+    shape_a = np.shape(a)
+    c = np.zeros(shape_a)
+    for i in range(shape_a[0]):
+            for j in range(shape_a[1]):
+                c[i,j] = b*a[i,j]
+    return c
+
+def produit(a,b):
+    shape_a = np.shape(a)
+    shape_b = np.shape(b)
+    c = np.zeros((shape_a[0],shape_b[1]))
+
+    if shape_a[1] != shape_b[0]:
+        print("colonnes(a) != lignes(b)")
+    else:
+        for i in range(shape_a[0]):
+            for j in range(shape_b[1]):
+                rowa = a[i,:]
+                colb = b[:,j]
+                c[i,j] = np.sum(rowa*colb)
+        return c
+
+def inversion(a):
+    shape_a = np.shape(a)
+    if shape_a != (2,2):
+        print("Pas une matrice 2x2")
+        return False
+    else:
+        det = a[0,0]*a[1,1]-(a[1,0]*a[0,1])
+        if det==0:
+            print("Dterminant égal à 0")
+            return False
+        else:
+            c = np.zeros(shape_a)
+            c[0,0]=a[1,1]
+            c[1,0]=-a[1,0]
+            c[0,1]=-a[0,1]
+            c[1,1]=a[0,0]
+            c = multiplication(c, 1/det)
+    return c
+
+def puissance(a,n):
+    res=a
+    for i in range(n-1):
+        print(a)
+        res = produit(res, a)
+    return res
 
 
-(A,B) = (np.array([[1,2],[3,4],[3,4]]), np.array([[1,2],[3,4],[3,4]]))
+
+A=np.array([[1,2],[3,4]])
+
+B=np.array([[5,6],[7,8]])
 
 print("\nEGALITE :")
 print(egalite(A,B))
@@ -73,3 +124,22 @@ print(addition(A,B))
 
 print("\nSOUSTRACTION :")
 print(soustraction(A,B))
+
+print("\n MULTIPLICATION PAR UN REEL :")
+print(multiplication(A,2))
+
+A=np.array([[5,6],[7,8]])
+
+B=np.array([[5,6,10],[7,8,9]])
+
+print("\nPRODUIT :")
+print(produit(A,B))
+
+print("\nINVERSE :")
+print(inversion(A))
+
+A=np.array([[5,6],[7,8]])
+b=3
+
+print("\nPUISSANCE :")
+print(puissance(A,b))
